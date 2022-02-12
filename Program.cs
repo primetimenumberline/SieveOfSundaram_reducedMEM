8df312812723c588b31b822129005aec2a817e60
primetimenumberline <primetimenumberline@gmail.com>
2022-02-12 17:35:59 -0400

    Adjusted the array by +1 so it  performs its search inclusive of n, as is done with the other sieves

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
 ﻿long n = 101;
-bool[] b = new bool[n/2];
-for (long i = 1; i < n; i++)
+bool[] b = new bool[n/2 + 1];
+for (long i = 1; i <= n; i++)
 {
-    for (long j = 1; (i+j+2*i*j) < n/2; j++)
+    for (long j = 1; (i+j+2*i*j) < n/2 + 1; j++)
     {
         b[i+j+2*i*j] = true;
     }
 }
 Console.WriteLine("2");
-for (long i = 1; i < n/2; i++)
+for (long i = 1; i < n/2 + 1; i++)
 {
     if (!b[i])
         Console.WriteLine("{0}", 2*i+1);
